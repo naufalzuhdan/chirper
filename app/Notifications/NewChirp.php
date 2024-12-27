85b57acd6bdbbe19cb2294a3073b59fe44e45913
Muhamad naufal zuhdan nawa <60124823+naufalzuhdan@users.noreply.github.com>
2024-12-27 19:48:32 +0700

    first init github

---
+++ app/Notifications/NewChirp.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Notifications;
+
+use App\Models\Chirp;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Str;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewChirp extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+
+    public function __construct(public Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject("New Chirp from {$this->chirp->user->name}")
+                    ->greeting("New Chirp from {$this->chirp->user->name}")
+                    ->line(Str::limit($this->chirp->message, 50))
+                    ->action('Go to Chirper', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+ 
+}
